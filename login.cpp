6b95c0963985a9af303b8a145f4cbad75814f833
XhuaSpy <xhuaspace@gmail.com>
2025-01-03 14:17:22 -0500

    login en github

--- login.cpp
+++ login.cpp
@@ -7,9 +7,8 @@ int main () {
     cout << "hola, soy un login" << endl;
     cout << "ahora hay una acualizacion en el log" << endl;
     cout << "otra modificacion bebesota" << endl;
-    cout << "otra modificacion bebesota" << endl;
-    cout << "otra modificacion bebesota" << endl;
-    cout << "otra modificacion bebesota" << endl;
+
+    cout << "Ahora estamos en github" << endl;
 
     return 0;
 }
